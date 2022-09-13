5d2f2021e6fd90f6a1335a96f8f3db6bdd67dfbe
Sean Finan <sean.finan@childrens.harvard.edu>
2022-09-13 14:53:05 -0400

    import cleanup, update/fix parameter error in pbj_receiver_v2.py

--- src/main/python/main_folder/example_tool2.py
+++ src/main/python/main_folder/example_tool2.py
@@ -5,11 +5,10 @@
 # These are the lines that ignore the typesystem errors
 import warnings
 
-import example_word_finder
 import pbj_receiver_v2
-import pbj_util
-from main_folder.pbj_sender_v2 import PBJSender
-from main_folder.pipeline import Pipeline
+from example_word_finder import ExampleWordFinder
+from pbj_sender_v2 import PBJSender
+from pipeline import Pipeline
 
 warnings.filterwarnings("ignore")
 
@@ -17,13 +16,13 @@ warnings.filterwarnings("ignore")
 def main():
     #  TODO - create an example that accepts command line parameters.
     #   e.g. main(argv) getopt("r:s:",["receive=","send="])
-    hostname = pbj_util.DEFAULT_HOST
-    port = pbj_util.DEFAULT_PORT
+    # hostname = pbj_util.DEFAULT_HOST
+    # port = pbj_util.DEFAULT_PORT
     queue_receive_cas = 'test/JavaToPython'
     queue_send_cas = 'test/PythonToJava'
 
     pipeline = Pipeline()
-    pipeline.add(example_word_finder.ExampleWordFinder())
+    pipeline.add(ExampleWordFinder())
     pipeline.add(PBJSender(queue_send_cas))
     pbj_receiver_v2.start_receiver(pipeline, queue_receive_cas)
 
